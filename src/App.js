27484eca56fc7b4b6e2c1f37229d46aee02a2f6d
Daniel Okobia <duokobia@gmail.com>
2023-01-23 12:02:33 +0100

    Week1 Exercises

---
+++ src/App.js
@@ -0,0 +1,68 @@
+import * as React from 'react';
+
+// const title = "React";
+// const welcome = {
+//   greeting: "Hey",
+//    title: "React",
+// };
+
+// function getTitle(title) {
+//   return title;
+// };
+
+const list = [
+  {
+    title: "React",
+    url: "https://reactjs.org/",
+    author: "Lordan walke",
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: "Redux",
+    url: "https://redux.js.org/",
+    author: "Dan Abramov, Andrew Clark",
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h1>My Hacker Stories </h1>
+
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
+
+      <hr />
+
+      <ul>
+        {list.map(function(item){
+          // By assisgning a key attribute to each list item's element,
+          // React can identify items if the list changes 
+          // (i.e if there is a re-ordering). It is best practice to use
+          // key as unique identifier rather than index of the array to
+          // ensure that the key is a stable(consistent) identifier. 
+          // Should the list change its order for example, React will not
+          // be able to identify the items properly when the index of the 
+          // array is used as key.
+          return <li key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+            </li>;
+        })}
+      </ul>
+     
+   
+    </div>
+  );
+}
+
+export default App;
