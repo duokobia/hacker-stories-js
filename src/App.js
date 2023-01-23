fb1e62b4ab191a727f37dcd1c392513192164e50
Daniel Okobia <duokobia@gmail.com>
2023-01-23 13:16:35 +0100

    Week2 Exercises

--- src/App.js
+++ src/App.js
@@ -29,40 +29,48 @@ const list = [
   },
 ];
 
-function App() {
-  return (
-    <div style={{ textAlign: 'center' }}>
-      <h1>My Hacker Stories </h1>
-
+//Extracting Search component
+const Search = () => (
+    <div>
       <label htmlFor="search">Search: </label>
       <input id="search" type="text" />
-
-      <hr />
-
-      <ul>
-        {list.map(function(item){
-          // By assisgning a key attribute to each list item's element,
-          // React can identify items if the list changes 
-          // (i.e if there is a re-ordering). It is best practice to use
-          // key as unique identifier rather than index of the array to
-          // ensure that the key is a stable(consistent) identifier. 
-          // Should the list change its order for example, React will not
-          // be able to identify the items properly when the index of the 
-          // array is used as key.
-          return <li key={item.objectID}>
-            <span>
-              <a href={item.url}>{item.title}</a>
-            </span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-            </li>;
-        })}
-      </ul>
-     
-   
     </div>
-  );
-}
+);
 
+//Extracting List component
+const List = () => (
+    <ul>
+    {list.map((item) => (
+      // By assisgning a key attribute to each list item's element,
+      // React can identify items if the list changes 
+      // (i.e if there is a re-ordering). It is best practice to use
+      // key as unique identifier rather than index of the array to
+      // ensure that the key is a stable(consistent) identifier. 
+      // Should the list change its order for example, React will not
+      // be able to identify the items properly when the index of the 
+      // array is used as key.
+      <li key={item.objectID}>
+        <span>
+          <a href={item.url}>{item.title}</a>
+        </span>
+        <span>{item.author}</span>
+        <span>{item.num_comments}</span>
+        <span>{item.points}</span>
+        </li>
+    ))}
+  </ul>
+);
+
+
+const App = () => (
+    <div style={{ textAlign: 'center' }}>
+      <h1>My Hacker Stories </h1>
+      <Search />
+      <hr />
+      <List />
+    </div>
+);
 export default App;
+
+
+
